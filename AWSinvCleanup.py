0a88539b5bc8f27c25ef2a0b80cc333a48632fe4
Charles O'Leary <ochuckles1@gmail.com>
2022-09-27 15:45:24 -0700

    AWSinvCleanup.py
    Cleanup script but written in python.

---
+++ AWSinvCleanup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 27 12:32:39 2022
+
+ Used to import, remove any quotations within th inventory, remove any
+ replicated key data, and then export as a CSV with the necessary
+ information. Some of this is necessary after curating a dataset inventory
+ on AWS Athena.
+ This will not work if you use versioning on AWS. A new variable will need
+ to be included in the import section, and a column will need to be
+ included in the export table.
+
+@author: cioleary
+"""
+
+# Define the function. It requires an input CSV to be passed to it.
+def AWSinvCleanup(inputCSV):
+    #%% Import necessary modules and the CSV
+    import pandas as pd
+    import numpy as np
+    # Import the CSV as a pandas dataframe
+    inputDF = pd.read_csv(inputCSV)
+    #%% Remove any "
+    # These can appear due to how AWS Athena handles strings
+    cleanedDF = inputDF.applymap(lambda x: x.replace('"', '')) 
+    #%% Create index 
+    # Find any instances of duplicated key values
+    idx = cleanedDF.duplicated(subset=['key'])
+    # Invert the index. This is due to the symantics of .duplicated 
+    idx = np.invert(idx)
+    # Reduce the DF based on the indexing
+    cleanddDF = cleanedDF[idx]
+    #%% Drop size field if it exists
+    if 'size' in cleanddDF.columns:  
+        cleanRedDF = cleanddDF.drop(['size'], axis=1)
+    else: cleanRedDF = cleanddDF
+    #%% Write the new pandas dataframe to CSV
+    # Give the CSV a "cleaned" name
+    outputCSV = "cleaned" + inputCSV
+    # Write the CSV uning the new name, without a header
+    cleanRedDF.to_csv(outputCSV,header=False,index=False)
